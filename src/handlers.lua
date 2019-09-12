b40149928e898f3487d5085f27dae0cca53f2d10
Alex Korotkov <durm.icecoffee@gmail.com>
2019-09-12 13:41:44 +0300

    formatting

--- src/handlers.lua
+++ src/handlers.lua
@@ -6,19 +6,9 @@ httpd = require("http.server").new(host, port)
 log = require('log')
 digest = require('digest')
 
-function resolveHandler(req)
-    log.info("resolve handler for method %s", req.method)
-    if req.method == "GET" then
-        return getValue(req)
-    elseif req.method == "PUT" then
-        return putValue(req)
-    elseif req.method == "DELETE" then
-        return deleteValue(req)
-    else
-        log.warn("method not allowed %s", req.method)
-        return methodNotAllowedError(req)
-    end
-end
+-------------
+-- errors
+-------------
 
 function methodNotAllowedError(req)
     return errorResponse(req, 405, 'Method not allowed')
@@ -46,6 +36,8 @@ function errorResponse(req, status, message)
     return resp
 end
 
+-------------
+
 function handleRequestLimit(req, h)
     local ip_addr = digest.md5(req.peer.host)
     local item = box.space.client:get{ip_addr}
@@ -71,60 +63,21 @@ function handleRequestLimit(req, h)
     return h(req)
 end
 
-function getValue(req)
-    local key = req:stash("id")
-    log.info("read - %s", key)
-    local item = box.space.kv:get{key}
-    if item == nil then
-        log.warn("not found... %s", key)
-        return notFoundError(req)
+function resolveHandler(req)
+    log.info("resolve handler for method %s", req.method)
+    if req.method == "GET" then
+        return getValue(req)
+    elseif req.method == "PUT" then
+        return putValue(req)
+    elseif req.method == "DELETE" then
+        return deleteValue(req)
+    else
+        log.warn("method not allowed %s", req.method)
+        return methodNotAllowedError(req)
     end
-    return req:render({json = item})
 end
 
-function postValue(req)
-    log.info("create")
-    local data = req:json()
-    log.debug("data: %s", data)
-    if data == nil or data.key == nil or data.value == nil then
-        log.warn("bad request...")
-        return badRequestError(req)
-    end
-    local item = box.space.kv:get{data.key} 
-    if item == nil then
-        item = box.space.kv:insert{data.key, data.value}
-        return req:render({json = item})
-    end
-    log.warn("conflict... %s", data.key)
-    return conflictError(req)
-end
-
-function putValue(req)
-    local key = req:stash("id")
-    log.info("update - %s", key)
-    local data = req:json()
-    log.debug("data: %s", data)
-    if data == nil or data.value == nil then
-        log.warn("bad request...")
-        return badRequestError(req)
-    end
-    local item = box.space.kv:get{key} 
-    if item then
-        item = box.space.kv:put{key, data.value}
-        return req:render({json = item})
-    end
-    log.warn("not found...")
-    return notFoundError(req)
-end
-
-function deleteValue(req)
-    local key = req:stash("id")
-    log.info("delete - %s", key)
-    local item = box.space.kv:get{key}
-    if item then 
-        box.space.kv:delete{key}
-    end
-end
+-------------
 
 httpd:route({ path = "/kv/:id" },  function (req) return handleRequestLimit(req, resolveHandler) end)
 httpd:route({ path = "/kv" }, function (req) return handleRequestLimit(req, postValue) end)
